8f074e120c270ec060cf82b880c26935966b4f99
d3meter <mark.demeter3@gmail.com>
2022-09-14 12:13:40 +0200

    add function load event

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,37 @@
-console.log("my first scrip file")
+let myVariable = "value";
+console.log(myVariable);
+
+myVariable = "new value";
+console.log(myVariable);
+
+myVariable = 2;
+console.log(myVariable);
+
+myVariable = true;
+console.log(myVariable);
+
+myVariable = "szöveg";
+console.log(myVariable);
+
+let myVariable2 = "szöveg";
+
+myVariable = myVariable2;
+console.log(myVariable);
+
+let myVariable3 = myVariable + myVariable2;
+console.log(myVariable3);
+
+function sumVariables(variable1, variable2) {
+    console.log(variable1 + variable2);
+}
+sumVariables(myVariable, myVariable2);
+sumVariables(8, "nyolc");
+sumVariables("word1","word2");
+
+function loadEvent() {
+    console.log("loaded");
+    let rooteElement = document.getElementById("root");
+    rooteElement.insertAdjacentHTML("beforeend", "hello world");
+}
+
+window.addEventListener("click", loadEvent);
